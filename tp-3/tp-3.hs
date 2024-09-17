f8191427090b24a248332ffc64b217a400fdebf2
ScheffelaarGerman <gscheffelaar@gmail.com>
2024-09-16 21:15:18 -0300

    #16 #17 #18 resueltos

--- tp-3/tp-3.hs
+++ tp-3/tp-3.hs
@@ -52,21 +52,21 @@ camino6 = Cofre [Cacharro, Tesoro](Cofre [Tesoro] (Cofre [Tesoro, Tesoro] Fin))
 hayTesoro :: Camino          -> Bool 
 hayTesoro    Fin             = False
 hayTesoro    (Nada c)        = hayTesoro c
-hayTesoro    (Cofre objts c) = existeTesoroEn objts || hayTesoro c 
+hayTesoro    (Cofre objts c) = existeAlMenosUnTesoroEn objts || hayTesoro c 
 
-existeTesoroEn:: [Objeto] -> Bool
-existeTesoroEn   []       =  False
-existeTesoroEn   (o: os)  =  sonIguales Tesoro o || existeTesoroEn os
+existeAlMenosUnTesoroEn :: [Objeto]  -> Bool
+existeAlMenosUnTesoroEn    []        = False
+existeAlMenosUnTesoroEn    (o : objs) = esTesoro o || existeAlMenosUnTesoroEn objs
 
-sonIguales :: Objeto -> Objeto -> Bool
-sonIguales    Tesoro    Tesoro = True
-sonIguales    _         _      = False
+esTesoro :: Objeto -> Bool
+esTesoro    Tesoro = True
+esTesoro    _      = False
 --
 --PRECONDICION: Debe haber al menos un tesoro.
 pasosHastaTesoro :: Camino              -> Int
 pasosHastaTesoro    Fin                 =  error " Debe existir al menos un tesoro"
 pasosHastaTesoro    (Nada c)             = 1 + pasosHastaTesoro c
-pasosHastaTesoro     (Cofre objts c) = if (existeTesoroEn objts)
+pasosHastaTesoro    (Cofre objts c) = if (existeAlMenosUnTesoroEn objts)
                                         then 0
                                         else 1 + pasosHastaTesoro c
 --
@@ -77,29 +77,43 @@ hayTesoroEn    n      (Nada c)         =  hayTesoroEn (n-1) c
 hayTesoroEn    n      (Cofre  objts c) =  hayTesoroEn (n-1) c
 
 hayTesoroAca :: Camino          -> Bool
-hayTesoroAca    (Cofre objts c) = existeTesoroEn objts
+hayTesoroAca    (Cofre objts c) = existeAlMenosUnTesoroEn objts
 hayTesoroAca    _               = False
 ---
---alMenosNTesoros :: Int  ->Camino        -> Bool
+--
+alMenosNTesoros :: Int  ->Camino         -> Bool
+alMenosNTesoros    0      _              = True
 alMenosNTesoros    _      Fin            = False
 alMenosNTesoros    n      (Nada c)       = alMenosNTesoros n c
-alMenosNTesoros    n      (Cofre objs c) = cantTesorosEnCofre objs >= n || alMenosNTesoros (n - cantTesorosEnCofre objs) c 
- 
-cantTesorosEnCofre :: [Objeto] -> Int
-cantTesorosEnCofre    []       =  0
-cantTesorosEnCofre    (ob: obs)  =  unoSi(sonIguales Tesoro ob) + cantTesorosEnCofre obs
+alMenosNTesoros    n      (Cofre objs c) = alMenosNTesoros (n - unoSi (existeAlMenosUnTesoroEn objs)) c
+
+
 --
+cantTesorosEntre :: Int ->  Int ->  Camino  ->  Int
+cantTesorosEntre     i       j        c     =   cantTesorosEnCamino (caminoDesde i (caminoHasta j c))
 
-cantTesorosEntre :: Int -> Int -> Camino -> Int
-cantTesorosEntre    n1     n2     c      = cantDeTesorosHasta (n2-1)  c - cantDeTesorosHasta n1 c
+caminoDesde ::  Int ->  Camino           ->  Camino
+caminoDesde     _       Fin              =  Fin
+caminoDesde     0       c                =  c
+caminoDesde     n       (Nada   c)       =  caminoDesde (n-1) c
+caminoDesde     n       (Cofre _ c)      =  caminoDesde (n-1) c
 
-cantDeTesorosHasta :: Int ->  Camino -> Int
-cantDeTesorosHasta    0       _               = 0
-cantDeTesorosHasta    _       Fin             = 0
-cantDeTesorosHasta    n       (Nada c)        = cantDeTesorosHasta (n-1) c
-cantDeTesorosHasta    n       (Cofre objts c) =  cantTesorosEnCofre objts  + cantDeTesorosHasta (n-1)  c                      
+caminoHasta ::  Int ->  Camino           ->  Camino
+caminoHasta     _       Fin              =   Fin
+caminoHasta     0       _                =   Fin
+caminoHasta     n       (Nada   c)       =   Nada (caminoHasta (n-1) c)
+caminoHasta     n       (Cofre objs c)   =   Cofre objs (caminoHasta (n-1) c)
+
+cantTesorosEnCamino ::  Camino  ->  Int
+cantTesorosEnCamino     Fin             =   0
+cantTesorosEnCamino     (Nada   c)      =   cantTesorosEnCamino c
+cantTesorosEnCamino     (Cofre objs c)  =  cantDeTesorosEn objs + cantTesorosEnCamino c
+
+cantDeTesorosEn :: [Objeto] -> Int
+cantDeTesorosEn    []       =  0
+cantDeTesorosEn   (ob: obs)  =  unoSi(esTesoro ob) + cantDeTesorosEn obs
+               
 --
-
 
 ------TIPOS ARBOREOS------
 --2.1 ARBOLES BINARIOS ---
@@ -227,31 +241,38 @@ expresion3 = Sum expresion1 expresion2
 expresion4 = Prod expresion1 expresion2
 expresion5 = Neg (expresion1)
 --2.1
-eval ::  ExpA            -> Int
-eval    (Valor n)        = n
-eval    (Sum  exp1 exp2) = eval exp1 + eval exp2
-eval    (Prod exp1 exp2) = eval exp1 * eval exp2
-eval    (Neg  exp1)      = -(eval exp1)
-
+eval :: ExpA                -> Int
+eval    (Valor  n)          =   n
+eval    (Sum    e1  e2)     =   eval e1   + eval    e2 
+eval    (Prod   e1  e2)     =   eval e1   * eval    e2
+eval    (Neg    e)         =   -   (eval e)
 --2.2
-simplificar :: ExpA -> ExpA
-simplificar (Valor n) = Valor n
-simplificar (Sum e1 e2) = simplificacionSum (simplificar e1) (simplificar e2)
-simplificar (Prod e1 e2) = simplificaProd (simplificar e1) (simplificar e2)
-simplificar (Neg e1) = simplificaNeg (simplificar e1)
 
-simplificaNeg :: ExpA -> ExpA
-simplificaNeg (Neg e1) = e1
-simplificaNeg e1 = (Neg e1)
+simplificar ::  ExpA    ->  ExpA
+simplificar    (Valor  n)          =   Valor    n
+simplificar    (Sum    e1  e2)     =   simplificarSuma (simplificar e1)(simplificar e2)
+simplificar    (Prod   e1  e2)     =   simplificarProducto (simplificar e1)(simplificar e2)
+simplificar    (Neg    e)          =   simplificarNegativo (simplificar e)
 
-simplificacionSum :: ExpA -> ExpA -> ExpA
-simplificacionSum e1 (Valor 0) = e1
-simplificacionSum (Valor 0) e2 = e2
-simplificacionSum e1 e2 = Sum e1 e2
+simplificarSuma ::  ExpA        ->  ExpA    ->  ExpA
+simplificarSuma     (Valor 0)   e2          =  e2
+simplificarSuma     e1          (Valor 0)   =  e1
+simplificarSuma     e1          e2          =  Sum e1 e2
 
-simplificaProd :: ExpA -> ExpA -> ExpA
-simplificaProd e1 (Valor 0) = (Valor 0)
-simplificaProd (Valor 0) e2 = (Valor 0)
-simplificaProd e1 (Valor 1) = e1
-simplificaProd (Valor 1) e2 = e2
-simplificaProd e1 e2 = Prod e1 e2
+simplificarProducto ::  ExpA        ->  ExpA       ->  ExpA
+simplificarProducto     (Valor 0)       _          =   Valor 0
+simplificarProducto     _              (Valor 0)   =   Valor 0
+simplificarProducto     (Valor 1)       e2         =   e2
+simplificarProducto     e1              (Valor 1)  =   e1   
+simplificarProducto     e1              e2         =   Prod e1  e2
+
+simplificarNegativo ::  ExpA        ->  ExpA
+simplificarNegativo     (Neg e)     =   e
+simplificarNegativo     e           =   (Neg e)
+
+
+
+
+
+
+
